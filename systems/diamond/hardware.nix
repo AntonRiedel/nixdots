bbd33221429f99de594ae937a96793a2fe5c4bee
Anton Riedel <anton.riedel@tum.de>
2024-09-28 23:34:43 +0200

    Feat: update diamond

--- systems/diamond/hardware.nix
+++ systems/diamond/hardware.nix
@@ -4,6 +4,10 @@
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
