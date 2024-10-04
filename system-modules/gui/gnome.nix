5060b93c1676efd183cf44afeea3fa28af4e179e
Anton Riedel <anton.riedel@tum.de>
2024-10-04 22:26:25 +0200

    Feat: update

--- system-modules/gui/gnome.nix
+++ system-modules/gui/gnome.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # Enable the GNOME desktop environment
   # it uses wayland by default even though it is defined under xserver
@@ -7,6 +7,25 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
+
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+      gedit
+    ])
+    ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      epiphany # web browser
+      geary # email reader
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+    ]);
 
   # disable pulseaudio explicitly
   hardware.pulseaudio.enable = false;
