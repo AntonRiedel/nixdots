714cc9933e8536b7ed93f13bd2df286a643a3326
Anton Riedel <anton.riedel@tum.de>
2024-10-09 17:06:56 +0200

    Feat: update

--- systems/silver/silver.nix
+++ systems/silver/silver.nix
@@ -6,21 +6,28 @@
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
   };
 
-  imports =
-    [
-      ../../home-modules/shell/bash.nix
-      ../../home-modules/shell/alias.nix
-      ../../home-modules/packages/cli-packages.nix
-      ../../home-modules/neovim/neovim.nix
-      ../../home-modules/tmux/tmux.nix
-      ../../home-modules/fzf/fzf.nix
-      ../../home-modules/git/git.nix
-    ];
+  imports = [
+    ../../home-modules/shell/bash.nix
+    ../../home-modules/shell/alias.nix
+    ../../home-modules/packages/cli-packages.nix
+    ../../home-modules/neovim/neovim.nix
+    ../../home-modules/tmux/tmux.nix
+    ../../home-modules/fzf/fzf.nix
+    ../../home-modules/git/git.nix
+  ];
 
   programs.direnv.enable = true;
+  home.sessionVariables = {
+    ALICE = "~/alice";
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
 }
