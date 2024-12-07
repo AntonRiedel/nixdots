831bc7a0c6c61b7ccbca5bd4d9f9132b62af334a
Anton Riedel <anton.riedel@tum.de>
2024-12-07 01:29:15 +0100

    Feat: update

--- systems/ruby/boot.nix
+++ systems/ruby/boot.nix
@@ -5,7 +5,15 @@
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.kernelPackages = pkgs.linuxPackages;
   # boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" "btrfs" ];
+  boot.initrd.availableKernelModules = [
+    "nvme"
+    "xhci_pci"
+    "usb_storage"
+    "sd_mod"
+    "rtsx_pci_sdmmc"
+    "btrfs"
+  ];
+  boot.initrd.systemd.enable = true;
   # boot.initrd.kernelModules = [ "amdgpu" ];
   # boot.kernelModules = [ "kvm-amd" ];
   # boot.extraModulePackages = [ ];
