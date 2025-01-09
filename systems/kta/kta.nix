c1165770f19f96eba8ba267cfa6ea33646e0f1d6
Anton Riedel <anton.riedel@tum.de>
2025-01-09 06:59:47 +0100

    Feat: update

--- systems/kta/kta.nix
+++ systems/kta/kta.nix
@@ -6,17 +6,20 @@
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
   };
 
-  imports =
-    [
-      ../../home-modules/shell/bash.nix
-      ../../home-modules/shell/alias.nix
-      ../../home-modules/packages/cli-packages.nix
-      ../../home-modules/neovim/neovim.nix
-      ../../home-modules/git/git.nix
-    ];
+  imports = [
+    ../../home-modules/shell/bash.nix
+    ../../home-modules/shell/alias.nix
+    ../../home-modules/packages/cli-packages.nix
+    ../../home-modules/neovim/neovim.nix
+    ../../home-modules/tmux/tmux.nix
+    ../../home-modules/git/git.nix
+  ];
 
   programs.direnv.enable = true;
   # Let Home Manager install and manage itself.
