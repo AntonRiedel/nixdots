3e35b6609720fc4cdb0732aab456f5cf1dfcbc75
Anton Riedel <anton.riedel@tum.de>
2025-02-10 10:21:01 +0100

    feat: update

--- systems/sapphire/home.nix
+++ systems/sapphire/home.nix
@@ -10,11 +10,12 @@
     # common modules
     ../../home-modules/packages/cli-packages.nix
     ../../home-modules/packages/gui-packages.nix
-    ../../home-modules/kitty/kitty.nix
+    #../../home-modules/kitty/kitty.nix
+    ../../home-modules/ghostty/ghostty.nix
+    ../../home-modules/keepassxc/keepassxc.nix
     ../../home-modules/shell/alias.nix
     ../../home-modules/shell/bash.nix
     ../../home-modules/git/git.nix
-    ../../home-modules/gpg/gpg.nix
     ../../home-modules/fzf/fzf.nix
     ../../home-modules/neovim/neovim.nix
   ];
