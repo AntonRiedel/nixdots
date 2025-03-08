22da785467c6b5afc150986f0fd0740b435b761d
Anton Riedel <anton.riedel@tum.de>
2025-03-08 17:58:41 +0100

    Feat: update

--- system-modules/dual-function-keys/dual-function-keys.nix
+++ system-modules/dual-function-keys/dual-function-keys.nix
@@ -1,12 +1,17 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   services.interception-tools = {
     enable = true;
     plugins = [ pkgs.interception-tools-plugins.dual-function-keys ];
     udevmonConfig = ''
-- JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
-  DEVICE:
-    NAME: ".*[Kk]eyboard.*"
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /etc/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          NAME: ".*[Kk]eyboard.*|.*[Kk]eychron.*"
     '';
   };
   environment.etc."dual-function-keys.yaml".text = builtins.readFile ./dual-function-keys.yaml;
