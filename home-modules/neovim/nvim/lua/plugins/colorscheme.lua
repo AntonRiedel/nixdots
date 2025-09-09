29a50e4dccd7e14007bf50403f7100e4dd010ff1
Anton Riedel <anton.riedel@tum.de>
2025-09-09 22:25:09 +0200

    Feat: update

--- home-modules/neovim/nvim/lua/plugins/colorscheme.lua
+++ home-modules/neovim/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,26 @@
 return {
 	-- the colorscheme should be available when starting Neovim
 	{
-		"EdenEast/nightfox.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
+		"maxmx03/solarized.nvim",
+		lazy = false,
+		priority = 1000,
+		---@type solarized.config
+		opts = {},
+		config = function(_, opts)
 			vim.o.termguicolors = true
-			vim.cmd([[colorscheme carbonfox]])
+			vim.o.background = "dark"
+			require("solarized").setup(opts)
+			vim.cmd.colorscheme("solarized")
 		end,
 	},
 }
+-- {
+-- 	"EdenEast/nightfox.nvim",
+-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- 	priority = 1000, -- make sure to load this before all the other start plugins
+-- 	config = function()
+-- 		-- load the colorscheme here
+-- 		vim.o.termguicolors = true
+-- 		vim.cmd([[colorscheme carbonfox]])
+-- 	end,
+-- },
