8d219a31fbb797ae82d7d25dff3db2efc3c9570f
Anton Riedel <anton.riedel@tum.de>
2026-01-24 23:14:41 +0100

    Feat: update

--- system-modules/packages/extra-packages.nix
+++ system-modules/packages/extra-packages.nix
@@ -3,14 +3,14 @@
   environment.systemPackages = with pkgs; [
     lutris
     mangohud
-    cool-retro-term
-    bambu-studio
   ];
 
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
   };
+
+  services.flatpak.enable = true;
 
   programs.gamemode.enable = true;
 
